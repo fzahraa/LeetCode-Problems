8c8f44ced9ad7e18fb79388f6d3187fdd39858bb
fatima tuz zahra <fzahra98@gmail.com>
2023-06-11 08:07:44 +0500

    LeetCode problems

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //MaximumProfit maximumProfit = new MaximumProfit();
+            //int[] prices = { 7, 1, 5, 3, 6, 4 };
+            //int res = maximumProfit.MaxProfit(prices);
+            //Console.WriteLine(res);
+
+            //RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+            //int[] nums = { 1, 2};
+            //res = removeDuplicates.RemoveDuplicatesFromSortedArray(nums);
+            //Console.WriteLine(res);
+
+            RotateArray rotateArray = new RotateArray();
+            int[] arr = { -1 };
+            //Method 1:
+            //rotateArray.RotateKTimes(arr, 3);
+
+            //Method 1:
+            //rotateArray.RotateUsingAuxiliaryArray(arr, 3);
+
+        }
+    }
+}
